f1040fd43bd3174da4981ff21492892eaaa61f36
melihorhanyilmaz <melih.yilmaz99@hotmail.com>
2023-05-22 12:30:20 +0200

    enter

---
+++ src/main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+columns = ['Construction year', 'Building condition', 'Number of frontages', 'Surroundings type', 'Living area', 'Living room surface', 'Kitchen type', 'Kitchen surface', 'Bedrooms', 'Bathrooms', 'Shower rooms', 'Toilets', 'Basement', 'Furnished', 'Terrace', 'Elevator', 'Accessible for disabled people', 'Swimming pool', 'Energy class', 'Heating type', 'Double glazing', 'Price', 'Address', 'Covered parking spaces', 'Flood zone type', 'Neighbourhood or locality', 'Outdoor parking spaces', 'Garden surface', 'Terrace surface', 'Garden', 'Monthly charges']
+
+old_csv = pd.read_csv('.\\assets\immoweb_properties_data.csv')
+
+good_csv = old_csv[old_csv["url"].str.contains("new-real-estate-project")==False]
+
+new_columns = good_csv[columns]
+
+purified_csv = new_columns.to_csv('.\\assets\\new_csv.csv')
